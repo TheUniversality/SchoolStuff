1ae81615cfce09c3003e950252f5bf49c1e413d4
TheUniversality <153824252+TheUniversality@users.noreply.github.com>
2024-10-10 09:35:08 +0200

    Update and rename Main.java to NumbQuess.java

--- NumbQuess.java
+++ NumbQuess.java
@@ -2,7 +2,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main
+public class NumbQuess
 {
 	public static void main(String[] args)
 	{
